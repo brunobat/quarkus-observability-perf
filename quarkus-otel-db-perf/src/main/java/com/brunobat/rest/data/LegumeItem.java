23938e68744597cbe57da21d6453bf6d0d737d67
brunobat <brunobat@gmail.com>
2024-07-18 11:42:44 +0100

    remove lombok

--- quarkus-otel-db-perf/src/main/java/com/brunobat/rest/data/LegumeItem.java
+++ quarkus-otel-db-perf/src/main/java/com/brunobat/rest/data/LegumeItem.java
@@ -1,17 +1,7 @@
 package com.brunobat.rest.data;
 
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@EqualsAndHashCode(of = {"id"})
-@Builder
 public class LegumeItem {
     private String id;
 
@@ -19,4 +9,100 @@ public class LegumeItem {
     private String name;
 
     private String description;
+
+    public LegumeItem(String id, @NotBlank String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public LegumeItem() {
+    }
+
+    public static LegumeItemBuilder builder() {
+        return new LegumeItemBuilder();
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public @NotBlank String getName() {
+        return this.name;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(@NotBlank String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String toString() {
+        return "LegumeItem(id=" + this.getId() + ", name=" + this.getName() + ", description=" + this.getDescription() + ")";
+    }
+
+    public boolean equals(final Object o) {
+        if (o == this) return true;
+        if (!(o instanceof LegumeItem)) return false;
+        final LegumeItem other = (LegumeItem) o;
+        if (!other.canEqual((Object) this)) return false;
+        final Object this$id = this.getId();
+        final Object other$id = other.getId();
+        if (this$id == null ? other$id != null : !this$id.equals(other$id)) return false;
+        return true;
+    }
+
+    protected boolean canEqual(final Object other) {
+        return other instanceof LegumeItem;
+    }
+
+    public int hashCode() {
+        final int PRIME = 59;
+        int result = 1;
+        final Object $id = this.getId();
+        result = result * PRIME + ($id == null ? 43 : $id.hashCode());
+        return result;
+    }
+
+    public static class LegumeItemBuilder {
+        private String id;
+        private @NotBlank String name;
+        private String description;
+
+        LegumeItemBuilder() {
+        }
+
+        public LegumeItemBuilder id(String id) {
+            this.id = id;
+            return this;
+        }
+
+        public LegumeItemBuilder name(@NotBlank String name) {
+            this.name = name;
+            return this;
+        }
+
+        public LegumeItemBuilder description(String description) {
+            this.description = description;
+            return this;
+        }
+
+        public LegumeItem build() {
+            return new LegumeItem(this.id, this.name, this.description);
+        }
+
+        public String toString() {
+            return "LegumeItem.LegumeItemBuilder(id=" + this.id + ", name=" + this.name + ", description=" + this.description + ")";
+        }
+    }
 }
