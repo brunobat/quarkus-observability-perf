82d07a99632e45f7ff2fc82f959dde09ab882fc6
Yoann Rodière <yoann@hibernate.org>
2025-09-10 13:23:47 +0200

    Use a StatelessSession (through Jakarta Data) for DB access
    Because our scenario is simple CRUD, without any association,
    it does not leverage the persistence context at all:
    we always do just one DB operation for every HTTP request.
    
    For that reason, **in this scenario**,
    a `StatelessSession` has a good chance to perform better:
    it'll do the same operations in DB, but will not try to maintain
    a persistence context and will thus avoid all associated overhead
    (mainly memory allocations).
    
    Note we kind of bypass Panache at the moment because StatelessSession
    will only be available in [Panache 2](https://github.com/quarkusio/quarkus/issues/46096).
    So for now we use Jakarta Data, which is a perfectly acceptable
    alternative in this specific scenario.

--- quarkus-otel-db-perf/src/main/java/com/brunobat/rest/LegumeRepository.java
+++ quarkus-otel-db-perf/src/main/java/com/brunobat/rest/LegumeRepository.java
@@ -2,24 +2,19 @@ package com.brunobat.rest;
 
 import com.brunobat.rest.data.LegumeItem;
 import com.brunobat.rest.model.Legume;
-import io.quarkus.hibernate.orm.panache.PanacheRepository;
-import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.inject.Inject;
-import jakarta.persistence.EntityManager;
+import jakarta.data.page.PageRequest;
+import jakarta.data.repository.CrudRepository;
+import jakarta.data.repository.Query;
+import jakarta.data.repository.Repository;
 
 import java.util.stream.Stream;
 
-@ApplicationScoped
-public class LegumeRepository implements PanacheRepository<Legume> {
+@Repository
+public interface LegumeRepository extends CrudRepository<Legume, Long> {
 
-    @Inject
-    EntityManager manager;
-
-    public Stream<LegumeItem> listLegumes(int pageIndex) {
-        return find("FROM Legume h")
-//                .withHint("org.hibernate.cacheable", "true")
-                .project(LegumeItem.class)
-                .page(pageIndex, 16).stream();
-    }
+	// In Jakarta Data 1.1, we will be able to replace this with:
+	// @Find(Legume.class)
+	@Query("SELECT h.id, h.name, h.description FROM Legume h")
+	Stream<LegumeItem> listLegumes(PageRequest pageRequest);
 
 }
